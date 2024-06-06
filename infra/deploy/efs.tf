17eca45a8919adf2e5d3ec23e7380db32284b5e2
Atunje <atunje_afolabi@yahoo.com>
2024-06-06 17:43:56 +0200

    add EFS access point

--- infra/deploy/efs.tf
+++ infra/deploy/efs.tf
@@ -35,3 +35,15 @@ resource "aws_efs_mount_target" "media_b" {
   subnet_id       = aws_subnet.private_b.id
   security_groups = [aws_security_group.efs.id]
 }
+
+resource "aws_efs_access_point" "media" {
+  file_system_id = aws_efs_file_system.media.id
+  root_directory {
+    path = "/api/media"
+    creation_info {
+      owner_gid   = 101
+      owner_uid   = 101 # W3: owner_uid should match the django-user uid defined in the dockerfile.
+      permissions = "755"
+    }
+  }
+}
