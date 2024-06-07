92071ceb104cc9f6724c3d33d70ca81a36ad1c24
Atunje <atunje_afolabi@yahoo.com>
2024-06-07 13:17:03 +0200

    add certificate
    * certificate validation also added

--- infra/deploy/dns.tf
+++ infra/deploy/dns.tf
@@ -10,3 +10,45 @@ resource "aws_route53_record" "app" {
 
   records = [aws_lb.api.dns_name]
 }
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = aws_route53_record.app.name
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+#####################################################################
+# W3: This resource block creates multiple route53 records          #
+# because of the for_loop (for each domain_validation_options).     #
+#                                                                   #
+# A list of domain_validation_options is returned after creating a  #
+# certificate through aws_acm_certificate above.                    #
+#####################################################################
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.zone.zone_id
+}
+
+###############################################################
+# W3: The actual certificate validation is performed          #
+# here using the fqdn for each route53 record created above.  #
+###############################################################
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
